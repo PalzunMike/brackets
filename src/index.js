ce974f21cd074dbb1b9f430423a4ccc305259fb0
Mike Palzun <palzun@gmail.com>
2020-10-15 18:16:46 +0300

    feat: task complied

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  let brackets = bracketsConfig.flat();
+
+  for (let i = 0; i <newStr.length; i++) {
+    for (let j = 0; j < brackets.length; j++) {
+      if (brackets[j] === newStr[i] && brackets[j+1] === newStr[i + 1]) {
+        newStr = newStr.slice(0, i) + newStr.slice(i + 2);
+      };
+    };
+  };
+
+  if (newStr.length === 0) {
+    return true;
+  };
+
+  if (str === newStr) {
+    return false;
+  } else {
+    return check(newStr, bracketsConfig);
+    };  
 }
